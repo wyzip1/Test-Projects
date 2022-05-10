ef3f150ec8a0881d9f696fe6779a025886fbf2a7
wyzip <2362086452@qq.com>
2022-05-10 14:26:27 +0800

    完成文件切片上传，可暂停继续

--- FileSliceUpload/index.js
+++ FileSliceUpload/index.js
@@ -1,23 +1,43 @@
+let uploadOriginFile;
+let uploadIndex;
+let controllAbort = new AbortController();
+let signal = controllAbort.signal
+let fileUid;
+
 function uploadFile(el) {
   const file = el.files[0];
-  uploadSlice(file, 999999999999)
+  uploadOriginFile = file
+  fileUid = guid();
+  uploadSlice(file, 1024 * 100)
 }
 
-function uploadSlice(file, sliceSize = 1024) {
-  const upload = (startIndex = 0) => {
+function uploadSlice(file, sliceSize = 1024, startIndex = 0) {
+  
+  const upload = (startIndex) => {
     const { blobFile, isLast } = sliceFile(file, startIndex, sliceSize)
     const formData = new FormData();
     formData.append('file', blobFile)
     fetch('uploadFile', {
       method: 'post',
-      body: formData
-    }).then(res => res.text()).then(info => {
-      console.log(info);
-      if(isLast) return console.log('所有切片上传完毕');
-      upload(startIndex + 1)
+      body: formData,
+      headers: {
+        fileuid: fileUid,
+        uploadend: isLast ? 'isLast' : 'continue'
+      },
+      signal
+    }).then(res => res.json()).then(info => {
+      log(startIndex + 1, Math.ceil(file.size / sliceSize))
+      if(isLast) {
+        uploadOriginFile = null;
+        uploadIndex = null;
+        fileUid = null;
+        return console.log('所有切片上传完毕', info);
+      }
+      uploadIndex = startIndex + 1
+      upload(uploadIndex)
     })
   }
-  upload()
+  upload(startIndex)
 }
 
 function sliceFile(file, index, sliceSize) {
@@ -29,4 +49,26 @@ function sliceFile(file, index, sliceSize) {
   const blobFile = new File([blob], `${filename}${index}.${ext}`);
   const isLast = end > file.size;
   return { blobFile, isLast }
+}
+
+function guid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0,
+          v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+  });
+}
+
+function pause() {
+  controllAbort.abort()
+  controllAbort = new AbortController()
+  signal = controllAbort.signal
+}
+
+function uploadContinue() {
+  uploadSlice(uploadOriginFile, 1024 * 100, uploadIndex)
+}
+
+function log(current, total) {
+  console.log('上传进度:', `${current}/${total}`);
 }
--- FileSliceUpload/server.js
+++ FileSliceUpload/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const fs = require('fs')
+const { resolve, parse } = require('path')
+const { IncomingForm } = require('formidable')
 
 app.engine('html', require('ejs').__express)
 app.set('view engine', 'html'); 
@@ -11,25 +13,46 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+const fileCaches = {}
+
 app.post('/uploadFile', (req, res) => {
-  let chunk;
-  req.on('data', (data) => {
-    if(!chunk) chunk = data;
-    else {
-      console.log(chunk);
-      chunk.concat(data);
+  const form = new IncomingForm({ uploadDir: './upload' });
+  form.parse(req, (err, _, { file }) => {
+    if(err) throw err
+    const responseJson = { success: true, file }
+
+    const fileUid = req.headers.fileuid;
+    !fileCaches[fileUid] && (fileCaches[fileUid] = [])
+    const fileList = fileCaches[fileUid]
+    const uploadSliceFile = { 
+      filePath: resolve(__dirname, 'upload', file.newFilename),
+      originName: file.originalFilename, 
+      newName: file.newFilename 
     }
-  })
-  req.on('end', () => {
-    const filename = chunk.toString().match(/filename="(.+)"/)[1]
-    const buffer = Buffer.from(chunk, 'binary')
-    console.log(buffer);
-    fs.writeFileSync('./upload/' + filename, buffer)
-    console.log('end', chunk);
-    res.send('end')
+    fileList.push(uploadSliceFile)
+    
+    if(req.headers.uploadend === 'isLast') {
+      const { ext } = parse(uploadSliceFile.originName)
+      const filename = uploadSliceFile.newName + ext
+      combinFile(fileList, resolve(__dirname, 'files', filename))
+      responseJson.url = `http://localhost:3000/files/${filename}`
+      delete fileCaches[fileUid]
+    };
+    res.json(responseJson)
   })
 })
 
 app.listen(3000, () => {
   console.log('Server run at http://localhost:3000');
 })
+
+function combinFile(fileList, writePath) {
+  const bufferList = []
+  fileList.forEach(file => {
+    const filePath = file.filePath
+    bufferList.push(fs.readFileSync(filePath))
+    fs.unlinkSync(filePath)
+  })
+  const buffers = Buffer.concat(bufferList)
+  fs.writeFileSync(writePath, buffers)
+}
